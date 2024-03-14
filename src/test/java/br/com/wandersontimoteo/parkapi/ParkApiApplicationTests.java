ac8bc78b3b38a0603125d204ff97511834653808
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2024-03-13 22:07:54 -0400

    Created project

---
+++ src/test/java/br/com/wandersontimoteo/parkapi/ParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.parkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
