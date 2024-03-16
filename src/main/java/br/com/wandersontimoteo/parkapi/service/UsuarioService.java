85a6c9ecbfeb48e0f6dbc01e64dc03a8d0a51436
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2024-03-16 09:59:13 -0400

    Added POST method to create User

--- src/main/java/br/com/wandersontimoteo/parkapi/service/UsuarioService.java
+++ src/main/java/br/com/wandersontimoteo/parkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package br.com.wandersontimoteo.parkapi.service;
 
+import br.com.wandersontimoteo.parkapi.entity.Usuario;
 import br.com.wandersontimoteo.parkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,8 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
