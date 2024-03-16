9a37dcf0849be7c3ccea355de886578d6209c182
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2024-03-16 10:58:16 -0400

    Added GET method to search user by id

--- src/main/java/br/com/wandersontimoteo/parkapi/service/UsuarioService.java
+++ src/main/java/br/com/wandersontimoteo/parkapi/service/UsuarioService.java
@@ -2,9 +2,9 @@ package br.com.wandersontimoteo.parkapi.service;
 
 import br.com.wandersontimoteo.parkapi.entity.Usuario;
 import br.com.wandersontimoteo.parkapi.repository.UsuarioRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +15,12 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
     }
 }
